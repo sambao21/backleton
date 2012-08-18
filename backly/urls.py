a6f8312f2de636b9f00cd350e5f9658726e5052c
Sam Bao <sambao21@gmail.com>
2012-08-17 22:33:51 -0500

    init commit

---
+++ backly/urls.py
@@ -0,0 +1,22 @@
+from backlyapp.views import HomeView
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'backly.views.home', name='home'),
+    # url(r'^backly/', include('backly.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^/?$', HomeView.as_view(), name='overview'),
+)
+
+urlpatterns += staticfiles_urlpatterns()
